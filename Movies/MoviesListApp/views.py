c0254cd0e27a7e2d549f01d4a1d9cfe9633f6f7c
Asma Abufayah <aaha44@gmail.com>
2022-05-23 12:02:08 +0300

    names changed

--- Movies/MoviesListApp/views.py
+++ Movies/MoviesListApp/views.py
@@ -4,6 +4,10 @@ from django.shortcuts import render
 
 
 def index(request):
+    return render(request, 'base.html')
+
+def search(request):
+
     search = request.GET.get('search')
 
     return render(request, 'base.html',{'search':search})
